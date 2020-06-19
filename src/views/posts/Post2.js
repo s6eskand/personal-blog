82c6f4965df8bc1ca7625cdd08a1762ec019483d
Sam <sam.eskandar00@hotmail.com>
2020-06-19 17:23:52 -0400

    update styles

--- src/views/posts/Post2.js
+++ src/views/posts/Post2.js
@@ -17,7 +17,10 @@ function Post2() {
                     <img className="author-img" src={samProfile} alt=""/>
                     <div className="info-text">
                         <p><b>Written by: Sam Eskandar</b></p>
-                        <p><i>June 19 2020 &bull; 5 min read</i></p>
+                        <div className="info-specific">
+                            <p><i>June 19 2020 </i><span role="img" aria-label="books">&#128218; &#128218; &#128218;</span> </p>
+                            <p style={{margin: '3px'}}><i> 10 min read</i></p>
+                        </div>
                     </div>
                     <div className="social-icons">
                         <a href="https://github.com/s6eskand"><FaGithub id="github" size={30} /></a>
@@ -27,8 +30,15 @@ function Post2() {
                 <div className="main-img" >
                     <img src={reactHook} alt=""/>
                 </div>
-                <div>
-                    <p>This is a test</p>
+                <div className="main-content">
+                    <p>
+                        Fishing is no easy task. You have to use the right bait, cast the line properly, be able to
+                        pick up the slightest of movements, and fight to pull the fish out of the water. <br/>
+                        <br/>
+                        <br/>
+                        Just as you are reading this post, I was incredibly disappointed that React Hooks had nothing to
+                        do with fishing, and everything to do with React.
+                    </p>
                 </div>
             </div>
         </div>
