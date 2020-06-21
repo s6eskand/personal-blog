681ddced915b14295f8bab17ac0ecd6c73632321
Sam <sam.eskandar00@hotmail.com>
2020-06-21 02:00:44 -0400

    add mobile and desktop views

--- src/components/Blogcard.js
+++ src/components/Blogcard.js
@@ -6,21 +6,23 @@ import { Button } from '@material-ui/core';
 class Blogcard extends React.Component{
     render() {
         return(
-            <div className="container" style={{paddingTop: '50px'}}>
-                <div className="blogcard">
-                    <div className="row">
-                        <img className="blogcard-image" src={this.props.image} alt=""/>
-                        <div className="blogcard-text">
-                            <div className="row">
-                                <img style={{padding: '20px'}} className="blogcard-profile" src={profilePic} alt=""/>
-                                <div>
-                                    <h5 style={{paddingTop: '27px'}}>{this.props.issue}</h5>
-                                    <h5>Written by Sam Eskandar</h5>
+            <div className="blogcard-all">
+                <div className="container">
+                    <div className="blogcard">
+                        <div className="row">
+                            <img className="blogcard-image" src={this.props.image} alt=""/>
+                            <div className="blogcard-text">
+                                <div className="row">
+                                    <img className="blogcard-profile" src={profilePic} alt=""/>
+                                    <div>
+                                        <h5 className="blogcard-issue">{this.props.issue}</h5>
+                                        <h5>Written by Sam Eskandar</h5>
+                                    </div>
                                 </div>
+                                <h3 className="blogcard-title">{this.props.title}</h3>
+                                <p className="blogcard-date"><i>{this.props.date}</i></p>
+                                <p className="blogcard-about">{this.props.tags.map(tag => <Button className="tag-btn" variant="outlined">{tag}</Button>)}</p>
                             </div>
-                            <h3 className="blogcard-title">{this.props.title}</h3>
-                            <p className="blogcard-date"><i>{this.props.date}</i></p>
-                            <p className="blogcard-about">{this.props.tags.map(tag => <Button className="tag-btn" variant="outlined">{tag}</Button>)}</p>
                         </div>
                     </div>
                 </div>
