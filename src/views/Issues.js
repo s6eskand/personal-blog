8ec87a7f645b2f2697e33de4243de841ee028123
Sam <sam.eskandar00@hotmail.com>
2020-06-21 02:00:04 -0400

    refactor to about

--- src/views/Issues.js
+++ src/views/Issues.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Post1 from './posts/Post1';
+import About from './posts/About';
 import Post2 from './posts/Post2';
 import Post3 from './posts/Post3';
 
@@ -9,7 +9,7 @@ class Issues extends React.Component {
     render() {
         return (
             <div>
-                <Route exact path="/issues/1" component={Post1} />
+                <Route exact path="/about" component={About} />
                 <Route exact path="/issues/2" component={Post2} />
                 <Route exact path="/issues/3" component={Post3} />
             </div>
