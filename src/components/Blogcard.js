1dbde2f0883a3a317051619f56f58041bcc070c3
Sam <sam.eskandar00@hotmail.com>
2020-06-21 16:59:19 -0400

    update for new routing

--- src/components/Blogcard.js
+++ src/components/Blogcard.js
@@ -3,32 +3,42 @@ import profilePic from '../media/images/blog_profile.png'
 import './Blogcard.css';
 import { Button } from '@material-ui/core';
 
-class Blogcard extends React.Component{
-    render() {
-        return(
-            <div className="blogcard-all">
-                <div className="container">
-                    <div className="blogcard">
-                        <div className="row">
-                            <img className="blogcard-image" src={this.props.image} alt=""/>
-                            <div className="blogcard-text">
-                                <div className="row">
-                                    <img className="blogcard-profile" src={profilePic} alt=""/>
-                                    <div>
-                                        <h5 className="blogcard-issue">{this.props.issue}</h5>
-                                        <h5>Written by Sam Eskandar</h5>
-                                    </div>
-                                </div>
-                                <h3 className="blogcard-title">{this.props.title}</h3>
-                                <p className="blogcard-date"><i>{this.props.date}</i></p>
-                                <p className="blogcard-about">{this.props.tags.map(tag => <Button className="tag-btn" variant="outlined">{tag}</Button>)}</p>
-                            </div>
+function Blogcard(props) {
+
+    const renderBooks = () => {
+        switch (props.books) {
+            case 1:
+                return <span role="img" aria-label="books">&#128218;</span>;
+            case 2:
+                return <span role="img" aria-label="books">&#128218; &#128218;</span>;
+            case 3:
+                return <span role="img" aria-label="books">&#128218; &#128218; &#128218;</span>;
+            case 4:
+                return <span role="img" aria-label="books">&#128218; &#128218; &#128218; &#128218;</span>;
+            case 5:
+                return <span role="img" aria-label="books">&#128218; &#128218; &#128218; &#128218; &#128218;</span>;
+            default:
+                return <span role="img" aria-label="books">&#128218;</span>;
+        }
+    };
+
+    return(
+        <div className="blogcard-all">
+            <div className="container">
+                <div className="blogcard">
+                    <div className="row">
+                        <img className="blogcard-image" src={props.image} alt=""/>
+                        <div className="blogcard-text">
+                            <h3 className="blogcard-title">{props.title}</h3>
+                            <p className="blogcard-about">{props.description}</p>
+                            <p className="blogcard-date"><i>{props.date} {renderBooks()} {props.time}</i></p>
+                            <p className="blogcard-about">{props.tags.map(tag => <Button className="tag-btn" variant="outlined">{tag}</Button>)}</p>
                         </div>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Blogcard;
